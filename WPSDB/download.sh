615545fd9efe04e725ecf8440c3c4ed6c9495fec
Tad <tad@spotco.us>
2021-04-09 16:22:49 -0400

    Reinitialized git repo
    This is a combination of ~8 commits.
    Original log follows:
    
    commit f2146e60fc8a4c767a82c14194a75e230b20a800 (HEAD -> master, origin/master)
    Author: Tad <tad@spotco.us>
    Date:   Fri Apr 9 12:17:03 2021 -0400
    
        Complete overhaul, long overdue
    
    commit 360ceb0341e7263ce856b2b56c14138c7caef198
    Author: Tad <tad@spotco.us>
    Date:   Tue Nov 17 10:49:35 2020 -0500
    
        License headers
    
    commit 861c8f053d65f44dc20edfc23a31e55846c9c93b
    Author: Tad <tad@spotco.us>
    Date:   Tue Jun 18 13:38:56 2019 -0400
    
        Going the distance... [pt2]
    
    commit d7575696e91a57e3927c9f9fd554126bbff295f4
    Author: Tad <tad@spotco.us>
    Date:   Thu May 9 06:53:38 2019 -0400
    
        Update license
    
    commit cc36d6ebae1d228ea582b49b0a5cab8d75707c05
    Author: Tad <tad@spotco.us>
    Date:   Sun Jun 3 14:04:29 2018 -0400
    
        Going the distance...
    
    commit a98564eefadd8adf569854089a392da2a9a8a80d
    Author: Tad <tad@spotco.us>
    Date:   Tue Aug 29 06:28:58 2017 -0400
    
        IntellIJ automated Inspect Code
    
    commit 92dc2f728bd77ec9446b4ce099712eabd8624d31
    Author: Tad <tad@spotco.us>
    Date:   Tue Aug 29 06:28:14 2017 -0400
    
        Formatting
    
    commit 109d64dbc54bf0df988258cc9ce5f6d3685f83a5
    Author: Tad <tad@spotco.us>
    Date:   Sun Jun 4 16:46:01 2017 -0400
    
        Fix copyright
    
    commit 3ace053a24271fc2ba711352d08dd261c7a0803a
    Author: Tad <tad@spotco.us>
    Date:   Fri Jun 2 03:52:43 2017 -0400
    
        Reinitialize git repo

---
+++ WPSDB/download.sh
@@ -0,0 +1,3 @@
+wget https://mylnikov.org/dump2019/wifi.csv -O Mylnikov.csv
+wget https://openwifi.su/db/db.csv.bz2 -O OpenWiFi.csv.bz2
+bzip2 -d OpenWiFi.csv.bz2
---
+++ src/MainN.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) 2015-2021 Divested Computing Group
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+/**
+ * Created using IntelliJ IDEA
+ * User: Tad
+ * Date: 3/3/16
+ * Time: 2:36 PM
+ */
+public class MainN {
+
+  private static final Map<String, String> databaseOut = new TreeMap<>();
+  private static String wpsdbDir = "";
+  private static int cTT = 0;
+  private static int cVT = 0;
+
+  private static int topLeftLat = 0;
+  private static int topLeftLon = 0;
+  private static int bottomRightLat = 0;
+  private static int bottomRightLon = 0;
+
+  public static void main(String[] args) {
+    System.out.println("Wi-Fi Database Merger");
+    System.out.println("Copyright 2015-2021 Divested Computing Group");
+    System.out.println("License: GPLv3\n");
+    if (args.length != 1) {
+      System.out.println("Please specify a config file");
+      System.exit(1);
+    }
+    File config = new File(args[0]);
+    if (config.exists()) {
+      System.out.println(config.getAbsoluteFile().getParent());
+      wpsdbDir = config.getAbsoluteFile().getParent();
+      executeConfig(config);
+    }
+  }
+
+  private static void executeConfig(File config) {
+    try {
+      Scanner scanner = new Scanner(config);
+      String line = "";
+      boolean hasBound = false;
+      while (scanner.hasNext()) {
+        line = scanner.nextLine();
+        if (line.startsWith("BOUND: ")) {
+          String[] lineArr = line.split(": ")[1].split(", ");
+          topLeftLat = Integer.valueOf(lineArr[0]);
+          topLeftLon = Integer.valueOf(lineArr[1]);
+          bottomRightLat = Integer.valueOf(lineArr[2]);
+          bottomRightLon = Integer.valueOf(lineArr[3]);
+          if (topLeftLat == 0 || topLeftLon == 0 || bottomRightLat == 0 || bottomRightLon == 0) {
+            System.out.println("Invalid bound");
+            System.exit(1);
+          } else {
+            hasBound = true;
+            System.out.println("Parsed bound");
+          }
+        }
+        if (line.startsWith("DATABASE: ")) {
+          if (hasBound) {
+            String[] lineArr = line.split(": ")[1].split("; ");
+            if (lineArr.length == 6) {
+              File db = new File(wpsdbDir + "/" + lineArr[0]);
+              if (db.exists()) {
+                String sep = lineArr[1];
+                int bssidPos = Integer.valueOf(lineArr[2]);
+                int latPos = Integer.valueOf(lineArr[3]);
+                int lonPos = Integer.valueOf(lineArr[4]);
+                boolean quotes = Boolean.valueOf(lineArr[5]);
+                System.out.println("Parsing " + db.getName());
+                processCSVDump(db, sep, bssidPos, latPos, lonPos, quotes);
+              } else {
+                System.out.println("Skipping unavailable database: " + lineArr[0]);
+              }
+            } else {
+              System.out.println("A database requires 6 arguments");
+              System.exit(1);
+            }
+          } else {
+            System.out.println("A bound must be specified before any databases");
+            System.exit(1);
+          }
+        }
+      }
+      File outDB = new File(config.toString().replaceAll(".cfg", "") + ".csv");
+      if (outDB.exists()) {
+        outDB.renameTo(new File(outDB.getName() + ".bak"));
+      }
+      writeDatabase(outDB);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void writeDatabase(File outDB) {
+    try {
+      PrintWriter out = new PrintWriter(outDB, "UTF-8");
+      for (Map.Entry<String, String> entry : databaseOut.entrySet()) {
+        String key = entry.getKey();
+        Object value = entry.getValue();
+        out.println(key + ":" + value);
+      }
+      out.close();
+      System.out.println("--- Finished ---");
+      System.out.println("Read " + cTT + " total entries");
+      System.out.println("Identified " + cVT + " total entries within range");
+      System.out.println("Wrote out " + databaseOut.size() + " entries to new CSV database");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void processCSVDump(File file, String sep, int bssidPos, int latPos, int lonPos,
+      boolean quotes) {
+    try {
+      int cT = 0;
+      int cV = 0;
+      int eS = databaseOut.size();
+      BufferedReader csv = new BufferedReader(new FileReader(file));
+      String line;
+      csv.readLine();
+      while ((line = csv.readLine()) != null) {
+        try {
+          line = line.replaceAll(sep, ":");
+          if (quotes) {
+            line = line.replaceAll("\"", "");
+          }
+          String[] wifi = line.split(":");
+          if (wifi.length >= 3) {
+            String bssid = wifi[bssidPos];
+            String lat = wifi[latPos];
+            String lon = wifi[lonPos];
+            if (bssid.length() == 12 && Double.valueOf(lat) >= bottomRightLat
+                && Double.valueOf(lat) <= topLeftLat && Double.valueOf(lon) >= topLeftLon
+                && Double.valueOf(lon) <= bottomRightLon) {
+              databaseOut.put(bssid, lat + ":" + lon);
+              cV++;
+            }
+          }
+          cT++;
+        } catch (Exception e1) {
+          // e1.printStackTrace();
+        }
+      }
+      csv.close();
+      System.gc();
+      cTT += cT;
+      cVT += cV;
+      System.out.println("\tRead " + cT + " entries");
+      System.out.println("\tIdentified " + cV + " entries within range");
+      System.out.println("\tAdded " + (databaseOut.size() - eS) + " entries");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
